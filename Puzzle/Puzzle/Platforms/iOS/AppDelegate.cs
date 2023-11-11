f92218ab2498e6ef24932d063ef559bc22ffa3c8
janobbb <jan.gumulka@edu.uekat.pl>
2023-11-12 00:57:07 +0100

    Puzzle upload v2

---
+++ Puzzle/Puzzle/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Puzzle;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ Puzzle/Puzzle/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Puzzle.MainApplication, Puzzle, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64a94af81ce9c7c5c6.MainApplication.class, crc64a94af81ce9c7c5c6.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
